7cac9799d55b2fb0ba128fa2c6e1cc2f817efff4
Hariharan <hariharan9572@gmail.com>
2026-01-14 23:03:24 +0530

    Creating Order Controller

---
+++ src/main/java/com/hariharan/springecom/service/OrderService.java
@@ -0,0 +1,19 @@
+package com.hariharan.springecom.service;
+
+import com.hariharan.springecom.model.dto.OrderRequest;
+import com.hariharan.springecom.model.dto.OrderResponse;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class OrderService {
+
+    public OrderResponse placeOrder(OrderRequest orderRequest) {
+        return null;
+    }
+
+    public List<OrderResponse> getAllOrderResponses() {
+        return null;
+    }
+}
