3b75865844a04e295e5d797ee94343b4149e5ca2
Hariharan <hariharan9572@gmail.com>
2026-01-26 15:03:31 +0530

    Building Service with ChatClient

--- src/main/java/com/hariharan/springecom/config/ChatClientConfig.java
+++ src/main/java/com/hariharan/springecom/config/ChatClientConfig.java
@@ -1,4 +1,4 @@
-package com.hariharan.springecom;
+package com.hariharan.springecom.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.context.annotation.Bean;
--- src/main/java/com/hariharan/springecom/service/ProductService.java
+++ src/main/java/com/hariharan/springecom/service/ProductService.java
@@ -2,6 +2,7 @@ package com.hariharan.springecom.service;
 
 import com.hariharan.springecom.model.Product;
 import com.hariharan.springecom.repo.ProductRepo;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -14,6 +15,9 @@ public class ProductService {
 
     @Autowired
     private ProductRepo productRepo;
+
+    @Autowired
+    private ChatClient chatClient;
 
     public List<Product> getAllProducts() {
         return productRepo.findAll();
@@ -41,6 +45,25 @@ public class ProductService {
 
     public String generateDescription(String name, String category) {
 
-        return "demo description";
+        String descPrompt = String.format("""
+                Write a concise and professional product description for an e-commerce listing.
+                
+                Product Name: %s
+                Category: %s
+
+                Keep it simple, engaging, and highlight its primary features or benefits.
+                Avoid technical jargon and keep it customer-friendly.
+                Limit the description to 250 characters maximum.
+                                
+                """, name, category);
+
+        String desc = chatClient.prompt(descPrompt)
+                .call()
+                .chatResponse()
+                .getResult()
+                .getOutput()
+                .getText();
+
+        return desc;
     }
 }
